28ad0358569336265daddfdbaa1e83a1163be786
Ananth-Pai19 <paiananth0838@gmail.com>
2026-05-21 03:04:47 +0530

    Updated some topic names

--- src/ros/topics.js
+++ src/ros/topics.js
@@ -3,10 +3,10 @@ export const TOPICS = {
   MOTION:      '/motion',
   ODOM:        '/zed/zed_node/odom',
   CONFIG:      '/config',
-  ARM:         '/enc_arm',
+  ARM:         '/enc_angles',
   GNSS:        '/gnss',
   ARM_STATE:   '/arm_state',
   INPUT_SPACE: '/input_space',
   ARM_PWM:     '/arm_target_angles',
-  DRIVE_PWM:   '/motor_pwm'
+  DRIVE_PWM:   '/drive_pwm'
 }
